78d8e87411519ced6ad8211c820c6b202313177a
CodingC1402 <CornyCodingCorn@gmail.com>
2022-05-04 10:21:51 +0700

    f

--- src/main/java/com/XiangQi/XiangQiBE/Configurations/CorseConfig.java
+++ src/main/java/com/XiangQi/XiangQiBE/Configurations/CorseConfig.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
