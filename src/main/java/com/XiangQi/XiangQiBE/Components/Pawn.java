2cc972b15aa562829f2a1cf1b5e3b5c1265047ee
CodingC1402 <CornyCodingCorn@gmail.com>
2022-05-10 18:00:51 +0700

    format

--- src/main/java/com/XiangQi/XiangQiBE/Components/Pawn.java
+++ src/main/java/com/XiangQi/XiangQiBE/Components/Pawn.java
@@ -4,22 +4,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Pawn extends Piece {
-  public String generateMove(String board, int x, int y, boolean isRed) {
+	public String generateMove(String board, int x, int y, boolean isRed) {
 		String result = "";
 		boolean crossedTheRiver = (isRed && y < 5) || (!isRed && y > 4);
-	
+
 		if (crossedTheRiver) {
 			result += generatePos(board, x + 1, y, isRed);
 			result += generatePos(board, x - 1, y, isRed);
 		}
-	
+
 		if (isRed) {
 			result += generatePos(board, x, y - 1, isRed);
-		}
-		else {
+		} else {
 			result += generatePos(board, x, y + 1, isRed);
 		}
-	
+
 		return result;
 	}
 }
